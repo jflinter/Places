769be9afe3d44548d51722949ad5cd7e5fc83d1f
Cameron Spickert <cameron@Camerons-MacBook-Air.local>
2014-04-17 00:44:04 -0400

    Initial Commit

---
+++ Places/PLCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PLCAppDelegate.h
+//  Places
+//
+//  Created by Cameron Spickert on 4/17/14.
+//  Copyright (c) 2014 Places. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Places/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Places
+//
+//  Created by Cameron Spickert on 4/17/14.
+//  Copyright (c) 2014 Places. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PLCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PLCAppDelegate class]));
+    }
+}
