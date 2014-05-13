fff7f051a949a9975159ab9a267fca6cbb02bc28
jflinter <jflinter11@gmail.com>
2014-05-13 15:34:38 -0400

    make a place's caption easier to read by overlaying it on a semitransparent white background

--- Places/PLCCalloutViewController.h
+++ Places/PLCCalloutViewController.h
@@ -13,6 +13,9 @@
 @interface PLCCalloutViewController : UIViewController<UITextViewDelegate>
 
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomSpacingConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *textViewLeadingConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *textViewWidthConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *textViewHeightConstraint;
 @property (weak, nonatomic) IBOutlet UIImageView *placeImageView;
 @property (weak, nonatomic) IBOutlet UITextView *captionTextView;
 @property (weak, nonatomic) IBOutlet UIToolbar *bottomToolbar;
