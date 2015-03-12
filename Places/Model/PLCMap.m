5719b52dacb4cc9fc58d9a90425b9659b7a0e43f
Jack Flintermann <jack@stripe.com>
2015-03-12 02:38:57 -0400

    remove url hashbang

--- Places/Model/PLCMap.m
+++ Places/Model/PLCMap.m
@@ -32,7 +32,7 @@
 }
 
 - (NSURL *)shareURL {
-    NSString *string = [NSString stringWithFormat:@"http://shareplac.es/#!/%@", self.urlId];
+    NSString *string = [NSString stringWithFormat:@"http://shareplac.es/%@", self.urlId];
     return [NSURL URLWithString:string];
 }
 
